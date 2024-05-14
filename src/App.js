f28e0896a5f37385ca5a61a05c2b217f6bf780be
Diogo de Paula <quintaodiogo@gmail.com>
2024-05-14 07:48:06 -0300

    Cria o menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { Fragment } from "react";
+import React from "react";
 import Sobre from './objects/Sobre/index.js'; // Fix the case of the file path
 import Logo from "./objects/Logo/index.js";
+import Menu from "./objects/Menu/index.js";
 
 function App() {
   return (
     <>
       <Logo/>
       <Sobre/>
+      <Menu/>
     </>
   );
 }
---
+++ src/objects/Menu/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import './style.css';
+
+const Menu = () => {
+    return (
+        <a href="#menu" className="menu"><span className="center">Menu</span></a>
+    );
+};
+export default Menu;
