19e7f778ab3653ea3cb25625690697498c35e846
Diogo de Paula <quintaodiogo@gmail.com>
2024-05-16 17:45:28 -0300

    Cria object Player

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Card from "./objects/Card";
+import Player from "./objects/Player";
 import Header from "./components/Header";
+
 function App() {
   return (
     <>
       <Header />
       <Card />
+      <Player />
     </>
   );
 }
---
+++ src/objects/Player/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./style.css";
+
+const Player = () => (
+    <button className="player">X</button>
+);
+export default Player;
