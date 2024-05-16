e67d27f735f5e848f037782ca86837af896b6dfd
Diogo de Paula <quintaodiogo@gmail.com>
2024-05-16 18:38:53 -0300

    Cria Player css

--- src/objects/Player/index.js
+++ src/objects/Player/index.js
@@ -1,7 +1,12 @@
 import React from "react";
+import playerX from "../../img/x.png"
+import playerO from "../../img/circle.png"
+
 import "./style.css";
 
 const Player = () => (
-    <button className="player">X</button>
+    <button className="player">
+        <img src={playerX} alt="Jogador X" />
+    </button>
 );
 export default Player;
