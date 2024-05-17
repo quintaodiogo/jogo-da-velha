2ebfa6f0a88f9d6f705c89e3b4bd1e484f667090
Diogo de Paula <quintaodiogo@gmail.com>
2024-05-17 12:17:55 -0300

    Cria grid da Hashtag

--- src/components/Hashtag/index.js
+++ src/components/Hashtag/index.js
@@ -4,19 +4,37 @@ import Card from "../../objects/Card";
 import Player from "../../objects/Player";
 
 const Hashtag = () => (
-    <Card> 
-        <Player player="x"/>
-        <Player player="o"/>
-        <Player player="x"/>
-
-        <Player player="o"/>
-        <Player player="x"/>
-        <Player player="o"/>
-
-        <Player player="x"/>
-        <Player player="o"/>
-        <Player player="x"/>
-    </Card>
+  <Card>
+    <ul className="hashtag">
+      <li className="item">
+        <Player player="x" />
+      </li>
+      <li className="item">
+        <Player player="o" />
+      </li>
+      <li className="item">
+        <Player player="x" />
+      </li>
+      <li className="item">
+        <Player player="o" />
+      </li>
+      <li className="item">
+        <Player player="x" />
+      </li>
+      <li className="item">
+        <Player player="o" />
+      </li>
+      <li className="item">
+        <Player player="x" />
+      </li>
+      <li className="item">
+        <Player player="o" />
+      </li>
+      <li className="item">
+        <Player player="x" />
+      </li>
+    </ul>
+  </Card>
 );
 
 export default Hashtag;
