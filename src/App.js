8124c06efd26c1c2b6e0f505f469de9638a4222f
Diogo de Paula <quintaodiogo@gmail.com>
2024-05-18 21:07:20 -0300

    Centraliza o nosso tabuleiro

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import Hashtag from "./components/Hashtag";
+import "./App.css";
 
 function App() {
   return (
-    <>
+    <main className="app">
       <Header />
       <Hashtag />
-    </>
+    </main>
   );
 }
 
