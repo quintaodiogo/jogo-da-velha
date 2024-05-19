7fa5143d7b887060b0e67bb05bf0d168becb570a
Diogo de Paula <quintaodiogo@gmail.com>
2024-05-18 21:26:54 -0300

    Cria css do Label

--- src/objects/Label/index.js
+++ src/objects/Label/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-
+import "./style.css";
 const Label = ({content}) => (
-    <label htmlFor="">{content}</label>
+    <label htmlFor="" className="label">{content}</label>
 );
 export default Label;
