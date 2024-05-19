5e98ce6a33b23980dd735e030e4c9b9e4504dd25
Diogo de Paula <quintaodiogo@gmail.com>
2024-05-18 21:21:55 -0300

    Cria object Label

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
+import "./App.css";
 import Header from "./components/Header";
 import Hashtag from "./components/Hashtag";
-import "./App.css";
+import Label from "./objects/Label";
 
 function App() {
   return (
     <main className="app">
       <Header />
       <Hashtag />
+      <Label content="Mostrar eventos"/>
     </main>
   );
 }
---
+++ src/objects/Label/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Label = ({content}) => (
+    <label htmlFor="">{content}</label>
+);
+export default Label;
