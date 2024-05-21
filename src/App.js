fd879c301d2f11a9b3e59206d3e0e75a941fc1eb
Diogo de Paula <quintaodiogo@gmail.com>
2024-05-21 10:13:11 -0300

    Cria o Object IconClose

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import "./App.css";
 import Checkbox from "./objects/Checkbox";
 import About from "./objects/About";
 import Sobre from "./objects/Sobre";
+import IconClose from "./objects/IconClose";
 import Header from "./components/Header";
 import Hashtag from "./components/Hashtag";
+
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
       <Checkbox value="show" id="show" content="Mostrar eventos" type="checkbox"/>
       <About>
         <Sobre className="-light"/>
+        <IconClose />
       </About>
     </main>
   );
---
+++ src/objects/IconClose/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import "./style.css";
+
+const IconClose = () => (
+    <a href="" className="icon-close">Fechar</a>
+);
+
+
+export default IconClose;
