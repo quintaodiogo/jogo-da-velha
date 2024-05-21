8853dd828facfd0104185957e434b58df751ba34
Diogo de Paula <quintaodiogo@gmail.com>
2024-05-20 21:24:21 -0300

    Cria o Object About

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./App.css";
+
+import Checkbox from "./objects/Checkbox";
+import About from "./objects/About";
 import Header from "./components/Header";
 import Hashtag from "./components/Hashtag";
-import Checkbox from "./objects/Checkbox";
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <Header />
       <Hashtag />
       <Checkbox value="show" id="show" content="Mostrar eventos" type="checkbox"/>
+      <About />
     </main>
   );
 }
---
+++ src/objects/About/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import "./style.css";
+
+const About = () => (
+    <article className='about'>Diogo de Paula</article>
+);
+export default About;
