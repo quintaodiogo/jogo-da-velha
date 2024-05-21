2d635b988e1ad9fa893816a0fd10f03b2ad6c55d
Diogo de Paula <quintaodiogo@gmail.com>
2024-05-21 18:48:25 -0300

    Cria Object AvatarProfile

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./App.css";
+import Profile from "./img/profile.png";
 
 import Checkbox from "./objects/Checkbox";
 import About from "./objects/About";
+import AvatarProfile from "./objects/AvatarProfile";
 import Header from "./components/Header";
 import Hashtag from "./components/Hashtag";
 import HeaderInternal from "./components/HeaderInternal";
@@ -15,6 +17,7 @@ function App() {
       <Checkbox value="show" id="show" content="Mostrar eventos" type="checkbox"/>
       <About>
         <HeaderInternal />
+        <AvatarProfile src={Profile} alt="Avatar do Diogo"/>
       </About>
     </main>
   );
---
+++ src/objects/AvatarProfile/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./style.css";
+
+const AvatarProfile = ({src,alt}) => (
+    <img src={src} alt={alt}/>
+);
+export default AvatarProfile;
