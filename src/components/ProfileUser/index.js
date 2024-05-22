c0b0c8cd08e9cab3f4f44340fd0b58044c0b5f41
Diogo de Paula <quintaodiogo@gmail.com>
2024-05-22 11:12:46 -0300

    Cria o Object IconSocial

--- src/components/ProfileUser/index.js
+++ src/components/ProfileUser/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./style.css";
 import Profile from "../../img/profile.png";
+import GitHubIcon from "../../img/github.png";
+import InstagramIcon from "../../img/instagram.png";
+import LinkedinIcon from "../../img/linkedin.png";
+import TwitterIcon from "../../img/twitter.png";
 
 import AvatarProfile from "../../objects/AvatarProfile";
+import IconSocial from "../../objects/IconSocial";
 
 const ProfileUser = () => (
   <dl className="profile-user">
@@ -16,6 +21,12 @@ const ProfileUser = () => (
     </dd>
     <dt className="title">Lorem ipsum</dt>
     <dd className="description">Lorem ipsum dolor sit amet. Aut voluptatem odit et assumenda nihil vel dolorem vitae et dignissimos illo. </dd>
+    <dd className="socials">
+      <IconSocial src={GitHubIcon} alt="Logo do GitHub"/>
+      <IconSocial src={InstagramIcon} alt="Logo do Instagram"/>
+      <IconSocial src={LinkedinIcon} alt="Logo do Linkedin"/>
+      <IconSocial src={TwitterIcon} alt="Logo do Twitter"/>
+    </dd>
   </dl>
 );
 export default ProfileUser;
---
+++ src/objects/IconSocial/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./style.css";
+
+const IconSocial = ({ src, alt }) => (
+  <img src={src} alt={alt} className="icon-social" />
+);
+export default IconSocial;
